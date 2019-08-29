65d514f1b891a4c697ace6a12761901ac3bf6136
Tuan Tu <tuantuchautrinh@gmail.com>
2019-08-29 10:08:23 +0700

    at Component Pane add Components: LeftPane, ContentPane and RightPane

---
+++ src/components/Pane/LeftPane/LeftPane.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LeftPane extends Component {
+    render() {
+        return (
+            <div className="panitem" id="left" />
+        );
+    }
+}
+
+export default LeftPane;
---
+++ src/components/Pane/RightPane/RightPane.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RightPane extends Component {
+    render() {
+        return (
+            <div className="panitem" id="right" />
+        );
+    }
+}
+
+export default RightPane;
