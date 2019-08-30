e81d4d0500ede808bb76a6aefd9caeee21a7999e
Tuan Tu <tuantuchautrinh@gmail.com>
2019-08-30 10:04:57 +0700

    Mapping Data vao Component use Main Concepts - Lists and Keys 'Console: Warning with key'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Product from '../Product/Product';
 import Modal from '../Modal/Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <Product price="1000000" images="/images/products/p1.png" hot={true}>Laptop Asus</Product>
-        <Product price="4000000" images="/images/products/p2.png" hot={true}>Điện thoại Asus</Product>
-        <Product price="5500000" images="/images/products/p3.png" hot={true}>iPhone X</Product>
-        <Product price="2500000" images="/images/products/p4.png" hot={true}>Macbook</Product>
-        <Product price="9600000" images="/images/products/p5.png" hot={true}>Laptop HP</Product>
-        <Product price="2600000" images="/images/products/p6.png" hot={true}>Laptop Lenovo</Product>
-      </div>
 
-      <Modal />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products :  [
+                    {
+                      name 	: "Laptop Asus",
+                      price 	: "10000000",
+                      images 	: "/images/products/p1.png",
+                      hot 	: true
+                    },
+                    {
+                      name 	: "Điện Thoại Asus",
+                      price 	: "40000000",
+                      images 	: "/images/products/p2.png",
+                      hot 	: false
+                    },
+                    {
+                      name 	: "Điện Thoại iPhone X",
+                      price 	: "55000000",
+                      images 	: "/images/products/p3.png",
+                      hot 	: true
+                    },
+                    {
+                      name 	: "Macbook",
+                      price 	: "55000000",
+                      images 	: "/images/products/p4.png",
+                      hot 	: true
+                    },
+                    {
+                      name 	: "Laptop HP",
+                      price 	: "96000000",
+                      images 	: "/images/products/p4.png",
+                      hot 	: false
+                    },
+                    {
+                      name 	: "Laptop Lenovo",
+                      price 	: "26000000",
+                      images 	: "/images/products/p3.png",
+                      hot 	: true
+                    }
+      ]
+    };
+  }
+
+  show_product = () => {
+    const listProduct = this.state.products.map((item) =>
+      <Product price={ item.price } images={ item.images } hot={true}>{ item.name }</Product>
+    );
+
+    return listProduct;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          { this.show_product() }
+        </div>
+
+        <Modal />
+      </div>
+    );
+  }
 }
 
 export default App;
