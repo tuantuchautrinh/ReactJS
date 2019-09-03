1ea48e33ad154f21d12e12632a608c1211bb50e3
Tuan Tu <tuantuchautrinh@gmail.com>
2019-09-03 14:01:08 +0700

    Tao Slug cho lien ket bang React Router with javascript-title-to-slug

--- src/components/Product/Item.js
+++ src/components/Product/Item.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
 class Item extends Component {
+    to_slug = (str) => {
+        // Chuyển hết sang chữ thường
+        str = str.toLowerCase();     
+    
+        // xóa dấu
+        str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
+        str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
+        str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
+        str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
+        str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
+        str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
+        str = str.replace(/(đ)/g, 'd');
+    
+        // Xóa ký tự đặc biệt
+        str = str.replace(/([^0-9a-z-\s])/g, '');
+    
+        // Xóa khoảng trắng thay bằng ký tự -
+        str = str.replace(/(\s+)/g, '-');
+    
+        // xóa phần dự - ở đầu
+        str = str.replace(/^-+/g, '');
+    
+        // xóa phần dư - ở cuối
+        str = str.replace(/-+$/g, '');
+    
+        // return
+        return str;
+    }
+
     render() {
         return (
             <div className="item  col-xs-4 col-lg-4">
                 <div className="thumbnail">
-                    <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                    <img className="group list-group-image" src="" alt="" />
                     <div className="caption">
                         <h4 className="group inner list-group-item-heading">
                             { this.props.children }
@@ -16,11 +45,11 @@ class Item extends Component {
                         <div className="row">
                             <div className="col-xs-12 col-md-8">
                                 <p className="lead">
-                                1.800.000 VNĐ
+                                { this.props.price } VNĐ
                                 </p>
                             </div>
                             <div className="col-xs-12 col-md-4">
-                                <a className="btn btn-success" href="http://www.jquery2dotnet.com">Chi tiết</a>
+                                <a className="btn btn-success" href={ "chi-tiet/" + this.props.pid + "/" + this.to_slug(this.props.children) + ".html" }>Chi tiết</a>
                             </div>
                         </div>
                     </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import Item from './Item';
+import myData from './data.json'
 
 class Product extends Component {
+    format_curency(price) {
+        return price.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+    }
+    
     render() {
         return (
             <div id="products" className="row list-group">
-                <Item>Lập Trình PHP</Item>
-                <Item>Lập Trình ASP</Item>
-                <Item>Lập Trình Java</Item>
-                <Item>Lập Trình Python</Item>
-                <Item>Lập Trình Android</Item>
-                <Item>Lập Trình Angular</Item>
+                {
+                    myData.map( (val, key) => {
+                        return <Item pid={ val.id } price={ this.format_curency(val.gia)} key={key}>{ val.ten }</Item>;
+                    })
+                }
             </div>
         );
     }
